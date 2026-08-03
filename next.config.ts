f3f99f7e386813aabf42c0e860fd8069e9fbc2d2
Vezzint <forcescircles@gmail.com>
2026-08-04 02:05:12 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: 'export', // 👈 Это самое важное — делает статический сайт
+  distDir: 'out',   // Папка, куда соберётся сайт
+  images: {
+    unoptimized: true, // Для GitHub Pages
+  },
+};
 
 export default nextConfig;
